3792d0bc7012c1cf46c4872b990ca38c0e7704b8
Sartajbir Singh <57433468+sartajsehgal@users.noreply.github.com>
2022-01-09 19:36:51 +0530

    Time: 34 ms (32.53%), Space: 17.5 MB (78.21%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.cpp
+++ 455-assign-cookies/455-assign-cookies.cpp
@@ -3,22 +3,15 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(s.begin(),s.end(),greater<int>());
         sort(g.begin(),g.end(),greater<int>());
-        // for(int i=0;i<s.size();i++)
-        // {
-        //     cout<<s[i]<<" ";
-        // }
-        // cout<<g.size()<<" "<<s.size()<<endl;
         int i=0,j=0,ans=0;
         while(i<g.size() and j<s.size())
         {
             if(s[j]>=g[i])
             {
                 ans++;
-                i++;
                 j++;
             }
-            else
-                i++;
+            i++;
         }
         return ans;
     }
