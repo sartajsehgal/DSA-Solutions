d6ff093b5c5a7caafa05ea125d788d29f0d895da
Sartajbir Singh <57433468+sartajsehgal@users.noreply.github.com>
2022-01-18 13:08:06 +0530

    Time: 8 ms (90.85%), Space: 9.3 MB (52.80%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum=0,zero=0,subsum;
+        for(int num:nums)
+        {
+            sum+=num;
+            if(num==0)
+                zero++;
+        }
+        float sub=0.5*(sum-target);
+        if(sub==floor(sub) and target<=sum)
+            subsum=sub;
+        else
+            return 0;
+        int n=nums.size();
+        int dp[n+1][subsum+1];
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<subsum+1;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j]=0;
+                }
+                if(j==0)
+                {
+                    dp[i][j]=1;
+                    // dp[i][j]=1;
+                }
+            }
+        }
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<subsum+1;j++)
+            {
+                if(nums[i-1]<=j and nums[i-1]!=0)
+                {
+                    dp[i][j]=dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return pow(2,zero)*dp[n][subsum];
+    }
+};
