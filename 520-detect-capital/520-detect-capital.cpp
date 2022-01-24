93242a5b748911d5df3bb3ea7ea663736afddc73
Sartajbir Singh <57433468+sartajsehgal@users.noreply.github.com>
2022-01-24 12:24:00 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (19.00%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        bool all_capitals=true,all_small=true,only_first=true;
+        for(int i=0;i<word.length();i++)
+        {
+            if(i==0 and (word[i]>='a' and word[i]<='z'))
+            {
+                only_first=false;
+            }
+            else if(i!=0 and (word[i]>='A' and word[i]<='Z'))
+            {
+                only_first=false;
+            }
+            if(word[i]>='a' and word[i]<='z')
+            {
+                all_capitals=false;
+            }   
+            else if(word[i]>='A' and word[i]<='Z')
+            {
+                all_small=false;
+            }
+        }
+        if(all_small or all_capitals or only_first)
+            return true;
+        return false;
+    }
+};
