9589fddf9c916c28bdbe47ae9a17a0a50b28ac99
Sartajbir Singh <57433468+sartajsehgal@users.noreply.github.com>
2022-01-31 00:04:36 +0530

    Time: 19 ms (40.81%), Space: 10.8 MB (89.04%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> comb;
+        //sort(candidates.begin(),candidates.end(),greater<int>());
+        helper(candidates,target,ans,comb,0);
+        return ans;
+    }
+    
+    void helper(vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& comb,int n)
+    {
+        // for(int a:comb)
+        // {
+        //     cout<<a<<" ";
+        // }
+        // cout<<endl;
+        if(target==0)
+        {
+            ans.push_back(comb);
+            return;
+        }
+        if(target<0)
+        {
+            return;
+        }
+        for(int i=n;i<candidates.size();i++)
+        {
+            comb.push_back(candidates[i]);
+            helper(candidates,target-candidates[i],ans,comb,i);
+            comb.pop_back();
+        }
+    }
+};
