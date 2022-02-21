dfd7503248d96815e92fb209a511b17e69dec0fe
Sartajbir Singh <57433468+sartajsehgal@users.noreply.github.com>
2022-02-21 11:34:04 +0530

    Time: 52 ms (5.23%), Space: 19.7 MB (52.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -5,12 +5,8 @@ public:
         for(int num : nums)
         {
             mp[num]++;
-        }
-        int n=nums.size();
-        for(auto x : mp)
-        {
-            if(x.second>floor(n/2))
-                return x.first;
+            if(mp[num]>floor(nums.size()/2))
+                return num;
         }
         return 0;
     }
