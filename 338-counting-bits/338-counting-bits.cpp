427fad0e785066ed2b3235abcbdd727709751924
Sartajbir Singh <57433468+sartajsehgal@users.noreply.github.com>
2022-03-01 11:10:05 +0530

    Time: 4 ms (88.16%), Space: 8.5 MB (21.58%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -11,13 +11,12 @@ public:
     
     int count_ones(int n)
     {
-        int count=0;
+        int ones=0;
         while(n!=0)
         {
-            if(n & 1)
-                count++;
-            n=n>>1;
+            ones+= (n & 1);
+            n=n >> 1;
         }
-        return count;
+        return ones;
     }
 };
