22dbb0559006a21135f55055be1eb54ac301fdb5
Sartajbir Singh <57433468+sartajsehgal@users.noreply.github.com>
2022-03-02 11:47:26 +0530

    Time: 3 ms (53.50%), Space: 7.6 MB (76.21%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,9 +3,12 @@ public:
     int rob(vector<int>& nums) {
         if(nums.size()==1)
             return nums[0];
-        for(int i=1;i<nums.size();i++)
+        if(nums.size()==2)
+            return max(nums[0],nums[1]);
+        nums[1]=max(nums[1],nums[0]);
+        for(int i=2;i<nums.size();i++)
         {
-            nums[i]=max(nums[i-1],nums[i]+(i>=2?nums[i-2]:0));
+            nums[i]=max(nums[i-1],nums[i]+nums[i-2]);
         }
         return nums.back();
     }
