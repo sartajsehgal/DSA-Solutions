ac7ec4dc466bc27757a37bd83abb643ea240545a
Sartajbir Singh <57433468+sartajsehgal@users.noreply.github.com>
2022-03-05 21:46:25 +0530

    Time: 7 ms (82.55%), Space: 10.2 MB (51.29%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for(int num:nums)
+        {
+            freq[num]++;
+        }
+        vector<pair<int,int>> arr;
+        for(auto f:freq)
+        {
+            arr.push_back(f);
+        }
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        vector<int> dp(n+1,-1);
+        return helper(arr,n-1,dp);
+    }
+    
+    int helper(vector<pair<int,int>>& arr,int n,vector<int>&dp)
+    {
+        if(n<0)
+            return 0;
+        if(dp[n]!=-1)
+            return dp[n];
+        if(n==0 or arr[n-1].first==arr[n].first-1)
+        {
+            return dp[n]=max(arr[n].first*arr[n].second+helper(arr,n-2,dp),helper(arr,n-1,dp));
+        }
+        return dp[n]=arr[n].first*arr[n].second+helper(arr,n-1,dp);
+    }
+};
