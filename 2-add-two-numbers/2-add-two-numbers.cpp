08a8a503cd49e495d1a7d6c18828d95fd710b4ef
Sartajbir Singh <57433468+sartajsehgal@users.noreply.github.com>
2022-03-10 11:56:47 +0530

    Time: 101 ms (5.34%), Space: 71.4 MB (50.05%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -14,38 +14,20 @@ public:
         ListNode *head=new ListNode();
         ListNode *ptr1=l1,*ptr2=l2,*ptr3=head;
         int carry=0;
-        while(ptr1!=NULL and ptr2!=NULL)
+        while(ptr1!=NULL or ptr2!=NULL)
         {
-            int sum = ptr1->val+ptr2->val+carry;
+            int v1=(ptr1 ? ptr1->val : 0);
+            int v2=(ptr2 ? ptr2->val : 0);
+            int sum = v1+v2+carry;
             int nsum = sum%10;
             sum/=10;
             carry=sum%10;
             ListNode *temp = new ListNode(nsum);
             ptr3->next=temp;
-            ptr1=ptr1->next;
-            ptr2=ptr2->next;
-            ptr3=ptr3->next;
-        }
-        while(ptr1!=NULL)
-        {
-            int sum=ptr1->val+carry;
-            int nsum = sum%10;
-            sum/=10;
-            carry=sum%10;
-            ListNode *temp = new ListNode(nsum);
-            ptr3->next=temp;
-            ptr1=ptr1->next;
-            ptr3=ptr3->next;
-        }
-        while(ptr2!=NULL)
-        {
-            int sum=ptr2->val+carry;
-            int nsum = sum%10;
-            sum/=10;
-            carry=sum%10;
-            ListNode *temp = new ListNode(nsum);
-            ptr3->next=temp;
-            ptr2=ptr2->next;
+            if(ptr1!=NULL)
+                ptr1=ptr1->next;
+            if(ptr2!=NULL)
+                ptr2=ptr2->next;
             ptr3=ptr3->next;
         }
         if(carry>0)
