99d7f4e9d68211e504aefcf1d6279bd8baea49eb
Sartajbir Singh <57433468+sartajsehgal@users.noreply.github.com>
2022-03-21 12:24:32 +0530

    Time: 6 ms (46.00%), Space: 6.7 MB (64.08%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -10,16 +10,11 @@ public:
         int st=0,e=last_occ[s[0]];
         for(int i=0;i<s.length();i++)
         {
-            if(i<e)
-            {
-                e=max(e,last_occ[s[i]]);
-            }
-            else if(i==e)
+            e=max(e,last_occ[s[i]]);
+            if(i==e)
             {
                 ans.push_back(e-st+1);
                 st=i+1;
-                if(i+1!=s.length())
-                    e=last_occ[s[i+1]];
             }
         }
         return ans;
