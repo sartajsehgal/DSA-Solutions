623e1b1ea5f92491cad60d4d7fb361ef163f16f9
Sartajbir Singh <57433468+sartajsehgal@users.noreply.github.com>
2022-04-21 12:06:32 +0530

    Time: 99 ms (83.04%), Space: 39.9 MB (92.56%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.cpp
@@ -0,0 +1,45 @@
+class MyHashSet {
+public:
+    vector<vector<int>> v;
+    int prime=123;
+    MyHashSet() {
+        for(int i=0;i<123;i++)
+        {
+            v.push_back({});
+        }
+    }
+    
+    void add(int key) {
+        int k=key%prime;
+        v[k].push_back(key);
+    }
+    
+    void remove(int key) {
+        int k=key%prime;
+        for(int i=0;i<v[k].size();i++)
+        {
+            if(v[k][i]==key)
+            {
+                v[k][i]=-1;
+            }
+        }
+    }
+    
+    bool contains(int key) {
+        int k=key%prime;
+        for(int i=0;i<v[k].size();i++)
+        {
+            if(v[k][i]==key)
+                return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
